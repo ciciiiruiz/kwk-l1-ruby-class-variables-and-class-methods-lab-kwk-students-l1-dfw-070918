2697f1946b0a386d3196980ff244fbdb4eeff35e
ciciiiruiz <ciciiiruiz@gmail.com>
2018-07-16 14:32:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,16 @@
 
 
 class Song
-  attr_accessor :name, :artists, :genres
+  attr_accessor :name, :artist, :genre
   @@count = 0
   def initialize name
     name = @name
+  end
+  def initialize artist
+    artist = @artist
+  end
+  def initialize genre
+    genre = @genre
   end
 end
 Song.new("99 Problems", "Jay-Z", "rap")
